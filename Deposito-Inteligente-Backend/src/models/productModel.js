e7de58a05f650115e5355f9316ce80663713845c
Matheus <matt-xp2@live.com>
2023-12-07 20:42:47 -0300

    :construction: Desenvolvendo o back-end

---
+++ Deposito-Inteligente-Backend/src/models/productModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const productSchema = new Schema({
+    code: {type: String, unique: true, require: true},
+    image: {type: String, require: true},
+    price: {type: Number, require: true},
+    quantity: {type: Number, require: true},
+    name: {type: String, require: true, unique: true},
+})
+
+module.exports = mongoose.model('Product', productSchema)
---
+++ Deposito-Inteligente-Backend/src/routers/productRouter.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const productController = require('../controllers/productController.js')
+const productRouter = express.Router()
+
+productRouter.route('/api/product')
+.get((req, res) => productController.search(req, res))
+.post((req, res) => productController.create(req, res))
+
+productRouter.route('/api/product/:code')
+.get((req, res) => productController.searchOne(req, res))
+
+productRouter.route('api/product/avatar')
+.post((req, res) => productController.changeAvatar(req, res))
+
+module.exports = productRouter
